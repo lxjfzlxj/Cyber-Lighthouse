9a0125d4ef1b20bf5cfc9f55c3d71ea54de0f513
lxjfzlxj <L_Star_Plus@qq.com>
2023-10-02 08:38:41 +0800

    feat(server): create a server cpp

--- Task1/server.cpp
+++ Task1/server.cpp
@@ -1,1 +1,15 @@
-//...
+#include <crow.h>
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")
+    ([]{
+        return "Hello, World!";
+    });
+
+    app.port(8080).multithreaded().run();
+
+    return 0;
+}
