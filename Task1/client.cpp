457ee03573eb0d0f38602797de7c4157eea2552e
lxjfzlxj <L_Star_Plus@qq.com>
2023-10-03 08:17:15 +0800

    feat(communication): send TCP/UDP package between client and server

--- Task1/client.cpp
+++ Task1/client.cpp
@@ -4,8 +4,22 @@
 #include <unistd.h>
 #include <cstring>
 const int DOMAIN = AF_INET;
-int main()
+char address[100];
+char sendData[100];
+char readData[100];
+int port;
+void InputAddressAndData()
 {
+	printf("IP address: ");
+	scanf("%s", address);
+	printf("port: ");
+	scanf("%d", &port);
+	printf("data: ");
+	scanf("%s", sendData);
+}
+void TCPwork()
+{
+	InputAddressAndData();
 	/*
 	create socket
 	domain
@@ -20,7 +34,7 @@ int main()
 	if (mySocket == -1)
 	{
 		fprintf(stderr, "Failed to create socket!\n");
-		return 1;
+		return;
 	}
 	else
 	{
@@ -29,24 +43,134 @@ int main()
 	// describe an Internet socket address
 	sockaddr_in serverAddress;
 	serverAddress.sin_family = DOMAIN;												   // address family
-	serverAddress.sin_addr.s_addr = inet_addr("0.0.0.0");							   // IP address
-	serverAddress.sin_port = htons(8080);											   // port
+	serverAddress.sin_addr.s_addr = inet_addr(address);								   // IP address
+	serverAddress.sin_port = htons(port);											   // port
 	int status = connect(mySocket, (sockaddr *)&serverAddress, sizeof(serverAddress)); // connect
 	// status	0: success	-1: failed
 	if (status < 0)
 	{
 		fprintf(stderr, "Failed to connect!\n");
-		return 2;
+		return;
 	}
 	else
 	{
 		printf("Succeed to connect!\n");
 	}
-	char *sendData = "12345";
-	char *readData;
-	send(mySocket, sendData, strlen(sendData), 0);
-	read(mySocket, readData, 100);
-	printf("%s\n", readData);
+	int sendStatus = send(mySocket, sendData, strlen(sendData), 0);
+	if (sendStatus < 0)
+	{
+		fprintf(stderr, "Failed to send data!\n");
+		return;
+	}
+	else
+	{
+		printf("Succeed to send data!\n");
+	}
+	sleep(3);
+	int readStatus = read(mySocket, readData, 100);
+	if (readStatus < 0)
+	{
+		fprintf(stderr, "Failed to read data!\n");
+		return;
+	}
+	else
+	{
+		printf("Succeed to read data!\n");
+	}
+	printf("result: %s\n", readData);
 	close(mySocket);
+}
+char udpString[10] = "UDPUDP";
+void UDPwork()
+{
+	InputAddressAndData();
+	int myTCPSocket = socket(DOMAIN, SOCK_STREAM, 0);
+	if (myTCPSocket == -1)
+	{
+		fprintf(stderr, "Failed to create TCP socket!\n");
+		return;
+	}
+	else
+	{
+		printf("Succeed to create TCP socket!\n");
+	}
+	int myUDPSocket = socket(DOMAIN, SOCK_DGRAM, 0);
+	if (myUDPSocket == -1)
+	{
+		fprintf(stderr, "Failed to create UDP socket!\n");
+		return;
+	}
+	else
+	{
+		printf("Succeed to create UDP socket!\n");
+	}
+	// describe an Internet socket address
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = DOMAIN;													  // address family
+	serverAddress.sin_addr.s_addr = inet_addr(address);									  // IP address
+	serverAddress.sin_port = htons(port);												  // port
+	int status = connect(myTCPSocket, (sockaddr *)&serverAddress, sizeof(serverAddress)); // connect
+	// status	0: success	-1: failed
+	if (status < 0)
+	{
+		fprintf(stderr, "Failed to connect(TCP)!\n");
+		return;
+	}
+	else
+	{
+		printf("Succeed to connect(TCP)!\n");
+	}
+	int sendStatus = send(myTCPSocket, udpString, strlen(udpString), 0);
+	if (sendStatus < 0)
+	{
+		fprintf(stderr, "Failed to send UDPUDP!\n");
+		return;
+	}
+	else
+	{
+		printf("Succeed to send UDPUDP!\n");
+	}
+	sleep(3); //
+	printf("senddata:%s\n", sendData);
+	sendStatus = sendto(myUDPSocket, sendData, strlen(sendData), MSG_CONFIRM, (const sockaddr *)&serverAddress, sizeof(serverAddress));
+	if (sendStatus < 0)
+	{
+		fprintf(stderr, "Failed to send data!\n");
+		return;
+	}
+	else
+	{
+		printf("Succeed to send data!\n");
+	}
+	sleep(3);
+	socklen_t socklen;
+	int recvStatus = recvfrom(myUDPSocket, readData, 100, MSG_WAITALL, (sockaddr *)&serverAddress, &socklen);
+	if (recvStatus < 0)
+	{
+		fprintf(stderr, "Failed to receive data!\n");
+		return;
+	}
+	else
+	{
+		printf("Succeed to receive data!\n");
+	}
+	printf("result: %s\n", readData);
+	close(myTCPSocket);
+	close(myUDPSocket);
+}
+int main()
+{
+	char chosenProtocol[10];
+	printf("Choose TCP/UDP protocol: ");
+	scanf("%s", chosenProtocol);
+	if (strcmp(chosenProtocol, "TCP") == 0)
+		TCPwork();
+	else if (strcmp(chosenProtocol, "UDP") == 0)
+		UDPwork();
+	else
+	{
+		fprintf(stderr, "Input invalid protocol!\n");
+		return 0;
+	}
 	return 0;
 }
--- Task1/server.cpp
+++ Task1/server.cpp
@@ -1,15 +1,64 @@
-#include <crow.h>
-
+#include <iostream>
+#include <boost/asio.hpp>
+using namespace std;
+using namespace boost;
+using namespace asio;
+using namespace asio::ip;
+const char udpString[10] = "UDPUDP";
+bool udpok;
+int port;
+void InputRequestAndPort() {
+	char Y_N;
+	printf("allow to receive udp package?(Y/N) ");
+	scanf("%c",Y_N);
+	if(Y_N=='Y') udpok=true;
+	else udpok=false;
+	printf("port: ");
+	scanf("%d",&port);
+}
 int main()
 {
-    crow::SimpleApp app;
+	InputRequestAndPort();
+	try
+	{
+		io_context ioContext;												  //管理各项网络任务
+		tcp::acceptor tcpAcceptor(ioContext, tcp::endpoint(tcp::v4(), 8080)); //获取客户端 ip+port
+		tcp::socket tcpSocket(ioContext);
+		tcpAcceptor.accept(tcpSocket); //接受客户端连接
+		printf("client IP:%s\n", tcpSocket.remote_endpoint().address().to_string().c_str());
+		bool useUDP = false;
+		while (1)
+		{
+			char data[100] = {0};
+			// buffer(...) 管理传入的内存，作为缓冲区，所有收发数据都要用这玩意
+			tcpSocket.receive(buffer(data)); // receive() 接受客户端发送的数据
+			puts(data);
+			if (strcmp(data, udpString) == 0)
+			{
+				if(udpok) {
+					useUDP = true;
+					break;
+				}
+			}
+			tcpSocket.send(buffer(data)); // send() 向客户端发送数据
+		}
+		if (useUDP)
+		{
+			io_context ioContext;
+			udp::socket udpSocket(ioContext, udp::endpoint(udp::v4(), 8080));
+			while (1)
+			{
+				char data[100] = {0};
+				udp::endpoint datafrom;
+				udpSocket.receive_from(buffer(data), datafrom);
+				udpSocket.send_to(buffer(data), datafrom);
+			}
+		}
+	}
+	catch (std::exception &e)
+	{
+		fprintf(stderr, "Exception: %s\n", e.what());
+	}
 
-    CROW_ROUTE(app, "/")
-    ([]{
-        return "Hello, World!";
-    });
-
-    app.port(8080).multithreaded().run();
-
-    return 0;
+	return 0;
 }
